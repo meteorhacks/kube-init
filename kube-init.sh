7c93a51896c392f3e373f048a90411ee87edb287
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2015-04-09 13:26:55 +0530

    Add DNS setup

--- kube-init.sh
+++ kube-init.sh
@@ -7,7 +7,7 @@ docker run \
     --net=host \
     -d kubernetes/etcd:2.0.5.1 \
     /usr/local/bin/etcd \
-        --addr=$HOSTNAME:4001 \
+        --addr=127.0.0.1:4001 \
         --bind-addr=0.0.0.0:4001 \
         --data-dir=/var/etcd/data
 
@@ -23,6 +23,8 @@ docker run \
         --address=0.0.0.0 \
         --enable_server \
         --hostname_override=127.0.0.1 \
+        --cluster_dns=10.0.0.10 \
+        --cluster_domain="kubernetes.local" \
         --config=/etc/kubernetes/manifests
 
 ## Proxy which changes IP Tables Rules
@@ -46,6 +48,92 @@ fi
 wget http://storage.googleapis.com/kubernetes-release/release/v0.14.1/bin/linux/$KUBECTL_ARCH/kubectl
 chmod +x kubectl
 mv kubectl /usr/local/bin
+
+## Add DNS Support
+cat <<EOF > /tmp/kube-dns-rc.yaml
+kind: ReplicationController
+apiVersion: v1beta1
+id: kube-dns
+namespace: default
+labels:
+  k8s-app: kube-dns
+  kubernetes.io/cluster-service: "true"
+desiredState:
+  replicas: 1
+  replicaSelector:
+    k8s-app: kube-dns
+  podTemplate:
+    labels:
+      name: kube-dns
+      k8s-app: kube-dns
+      kubernetes.io/cluster-service: "true"
+    desiredState:
+      manifest:
+        version: v1beta2
+        id: kube-dns
+        dnsPolicy: "Default"  # Don't use cluster DNS.
+        containers:
+          - name: etcd
+            image: quay.io/coreos/etcd:v2.0.3
+            command: [
+                    # entrypoint = "/etcd",
+                    "-listen-client-urls=http://0.0.0.0:2379,http://0.0.0.0:4001",
+                    "-initial-cluster-token=skydns-etcd",
+                    "-advertise-client-urls=http://127.0.0.1:4001",
+            ]
+          - name: kube2sky
+            image: gcr.io/google_containers/kube2sky:1.1
+            command: [
+                    # entrypoint = "/kube2sky",
+                    "-domain=kubernetes.local",
+            ]
+          - name: skydns
+            image: gcr.io/google_containers/skydns:2015-03-11-001
+            command: [
+                    # entrypoint = "/skydns",
+                    "-machines=http://localhost:4001",
+                    "-addr=0.0.0.0:53",
+                    "-domain=kubernetes.local.",
+            ]
+            ports:
+              - name: dns
+                containerPort: 53
+                protocol: UDP
+EOF
+
+cat <<EOF > /tmp/kube-dns-service.yaml
+kind: Service
+apiVersion: v1beta1
+id: kube-dns
+namespace: default
+protocol: UDP
+port: 53
+portalIP: 10.0.0.10
+containerPort: 53
+labels:
+  k8s-app: kube-dns
+  name: kube-dns
+  kubernetes.io/cluster-service: "true"
+selector:
+  k8s-app: kube-dns
+EOF
+
+kubectl create -f /tmp/kube-dns-rc.yaml
+kubectl create -f /tmp/kube-dns-service.yaml
+
+rm /tmp/kube-dns-rc.yaml /tmp/kube-dns-service.yaml
+
+echo
+echo "=> Waiting for DNS setup comes online. (takes upto 2-5 minute)"
+while [[ 1 ]]; do
+    #statements
+    ok=$(kubectl get pod -l k8s-app=kube-dns | grep "Running")
+    if [[ $ok ]]; then
+        break
+    fi
+    sleep 1
+done
+echo "=>  DNS confguration completed."
 
 ## Done
 echo
